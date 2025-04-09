5debadf877e9b2d8625007be06bb0851c8215782
rei.minegishi <rei.minegishi@gmail.com>
2025-04-09 20:23:35 +0900

    test

--- release.sh
+++ release.sh
@@ -1,24 +1,22 @@
+#!/bin/bash
+# filepath: /Users/minegishirei/myworking/master_tuner/release.sh
 
+set -e
 
+# Docker ボリュームとポートのマッピング設定
+VOLUMES="-v \"$(pwd)/code:/code\" \
+-v \"$(pwd)/volumes/static/master_tuner:/code/vue-notus/public/master_tuner\" \
+-v \"$(pwd)/volumes/static/master_tunerdict:/code/vue-notus/public/master_tunerdict\""
+PORTS="-p 3000:3000 -p 8080:8080 -p 19000:19000 -p 19001:19001 -p 19002:19002 -p 19006:19006"
+
+# Docker イメージのビルド
 docker build -t react-app ./code
 
-docker run --rm \
-  -v "$(pwd)/code:/code" \
-  -v "$(pwd)/volumes/static/master_tuner:/code/vue-notus/public/master_tuner" \
-  -v "$(pwd)/volumes/static/master_tunerdict:/code/vue-notus/public/master_tunerdict" \
-  -p 3000:3000 -p 8080:8080 -p 19000:19000 -p 19001:19001 -p 19002:19002 -p 19006:19006 \
-  react-app sh -c "cd /code/vue-notus && npm install"
+# npm install の実行
+docker run --rm ${VOLUMES} ${PORTS} react-app sh -c "cd /code/vue-notus && npm install"
 
-docker run --rm \
-  -v "$(pwd)/code:/code" \
-  -v "$(pwd)/volumes/static/master_tuner:/code/vue-notus/public/master_tuner" \
-  -v "$(pwd)/volumes/static/master_tunerdict:/code/vue-notus/public/master_tunerdict" \
-  -p 3000:3000 -p 8080:8080 -p 19000:19000 -p 19001:19001 -p 19002:19002 -p 19006:19006 \
-  react-app sh -c "cd /code/vue-notus && npm run build"
+# アプリケーションのビルド
+docker run --rm ${VOLUMES} ${PORTS} react-app sh -c "cd /code/vue-notus && npm run build"
 
-docker run --rm \
-  -v "$(pwd)/code:/code" \
-  -v "$(pwd)/volumes/static/master_tuner:/code/vue-notus/public/master_tuner" \
-  -v "$(pwd)/volumes/static/master_tunerdict:/code/vue-notus/public/master_tunerdict" \
-  -p 3000:3000 -p 8080:8080 -p 19000:19000 -p 19001:19001 -p 19002:19002 -p 19006:19006 \
-  react-app sh -c "cd /code/vue-notus && npm run serve"
+# アプリケーションの起動
+docker run --rm ${VOLUMES} ${PORTS} react-app sh -c "cd /code/vue-notus && npm run serve"
