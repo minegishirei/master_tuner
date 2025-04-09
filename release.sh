802f7c217874346ed06cea7b5e7225ba15b5b1bd
rei.minegishi <rei.minegishi@gmail.com>
2025-04-09 22:02:25 +0900

    test

--- release.sh
+++ release.sh
@@ -3,10 +3,10 @@
 
 set -e
 
-# Docker ボリュームとポートのマッピング設定
-VOLUMES="-v \"$(pwd)/code:/code\" \
--v \"$(pwd)/volumes/static/master_tuner:/code/vue-notus/public/master_tuner\" \
--v \"$(pwd)/volumes/static/master_tunerdict:/code/vue-notus/public/master_tunerdict\""
+# Docker ボリュームとポートのマッピング設定（引用符を削除）
+VOLUMES="-v $(pwd)/code:/code \
+-v $(pwd)/volumes/static/master_tuner:/code/vue-notus/public/master_tuner \
+-v $(pwd)/volumes/static/master_tunerdict:/code/vue-notus/public/master_tunerdict"
 PORTS="-p 3000:3000 -p 8080:8080 -p 19000:19000 -p 19001:19001 -p 19002:19002 -p 19006:19006"
 
 # Docker イメージのビルド
