cbece95ad05fe4039eb2d2cf34cd24adec8a5cd6
rei.minegishi <rei.minegishi@gmail.com>
2025-04-12 10:22:41 +0900

    test

--- code/vue-notus/src/views/admin/Dashboard2.vue
+++ code/vue-notus/src/views/admin/Dashboard2.vue
@@ -172,6 +172,7 @@ export default {
               new go.TextBlock({ font: '10pt Verdana, sans-serif', stroke: 'white' }).bind('text')
             )
         );
+      /*
         myDiagram.linkTemplate = new go.Link(
           { routing: go.Routing.Orthogonal, deletable: false, corner: 10, toShortLength: 4 }
         )
@@ -179,14 +180,137 @@ export default {
             new go.Shape({ strokeWidth: 2, stroke: 'white' }), // 白色のリンク
             new go.Shape({ toArrow: 'Standard', strokeWidth: 0, fill: 'white' }) // 白色の矢印
           );
-      myDiagram.layout = new go.LayeredDigraphLayout({
-          direction: 90, // 上から下に配置 (0: 左→右, 90: 上→下)
-          layerSpacing: 50, // レイヤー間のスペース
-          columnSpacing: 30 // 列間のスペース
-        });
+      */
+         // replace the default Link template in the linkTemplateMap
+    myDiagram.linkTemplate = new go.Link( // the whole link panel
+      {
+        // path animation works with these kinds of links too:
+        // routing: go.Routing.AvoidsNodes,
+        // curve: go.Curve.Bezier,
+      })
+        .add(
+          new go.Shape({ stroke: 'white' }), // the link shape
+          new go.Shape({ toArrow: 'standard', stroke: null,  fill: 'white' }), // the arrowhead
+          new go.Panel('Auto')
+            .add(
+              new go.Shape({ // the label background, which becomes transparent around the edges
+                fill: new go.Brush('Radial', { 0: 'rgb(240, 240, 240)', 0.3: 'rgb(240, 240, 240)', 1: 'rgba(240, 240, 240, 0)' }),
+                stroke: null
+              }),
+              new go.TextBlock({ // the label text
+                textAlign: 'center',
+                font: '10pt helvetica, arial, sans-serif',
+                stroke: '#555555',
+                margin: 4
+              })
+                .bind('text')
+            )
+        );
+    myDiagram.nodeTemplateMap.add(
+      'token',
+      new go.Part({ locationSpot: go.Spot.Center, layerName: 'Foreground' })
+        .add(
+          new go.Shape('Circle', { width: 12, height: 12, fill: 'green', strokeWidth: 0 }).bind('fill', 'color')
+        )
+    );
+      myDiagram.layout = new go.ForceDirectedLayout({ // automatically spread nodes apart
+        defaultElectricalCharge: 300,
+        defaultSpringLength: 150
+      }),
       myDiagram.model = go.Model.fromJson(psy_data);
         
-    },
+
+    function initTokens() {
+    var oldskips = myDiagram.skipsUndoManager;
+    myDiagram.skipsUndoManager = true;
+    myDiagram.model.addNodeDataCollection([
+      { category: 'token', at: 1, color: 'green' },
+      { category: 'token', at: 2, color: 'blue' },
+      { category: 'token', at: 4, color: 'yellow' },
+      { category: 'token', at: 5, color: 'purple' },
+      { category: 'token', at: 7, color: 'red' },
+      { category: 'token', at: 8, color: 'black' },
+      { category: 'token', at: 9, color: 'green' },
+      { category: 'token', at: 11, color: 'blue' },
+      { category: 'token', at: 12, color: 'yellow' },
+      { category: 'token', at: 17, color: 'purple' },
+      { category: 'token', at: 18, color: 'red' }
+    ]);
+    myDiagram.skipsUndoManager = oldskips;
+    window.requestAnimationFrame(updateTokens);
+  }
+    
+  function updateTokens() {
+    var oldskips = myDiagram.skipsUndoManager;
+    myDiagram.skipsUndoManager = true; // don't record these changes in the UndoManager!
+    var temp = new go.Point();
+    myDiagram.parts.each((token) => {
+      var data = token.data;
+      if (!data) return;
+      var at = data.at;
+      if (at === undefined) return;
+      var from = myDiagram.findNodeForKey(at);
+      if (from === null) return;
+      var frac = data.frac;
+      if (frac === undefined) frac = 0.0;
+      var next = data.next;
+      var to = myDiagram.findNodeForKey(next);
+      if (to === null) {
+        // nowhere to go?
+        positionTokenAtNode(token, from); // temporarily stay at the current node
+        data.next = chooseDestination(token, from); // and decide where to go next
+      } else {
+        // proceed toward the "to" port
+        var link = from.findLinksTo(to).first();
+        if (link !== null) {
+          token.location = link.path.getDocumentPoint(link.path.geometry.getPointAlongPath(frac, temp));
+        } else {
+          // stay at the current node
+          positionTokenAtNode(token, from);
+        }
+        if (frac >= 1.0) {
+          // if beyond the end, it's "AT" the NEXT node
+          data.frac = 0.0;
+          data.at = next;
+          data.next = undefined; // don't know where to go next
+        } else {
+          // otherwise, move fractionally closer to the NEXT node
+          data.frac = frac + 0.01;
+        }
+      }
+    });
+    myDiagram.skipsUndoManager = oldskips;
+    window.requestAnimationFrame(updateTokens);
+  }
+
+    // determine where to position a token when it is resting at a node
+    function positionTokenAtNode(token, node) {
+    // these details depend on the node template
+    token.location = node.position.copy().offset(4 + 6, 5 + 6);
+  }
+
+  function chooseDestination(token, node) {
+    var dests = new go.List().addAll(node.findNodesOutOf());
+    if (dests.count > 0) {
+      var dest = dests.elt(Math.floor(Math.random() * dests.count));
+      return dest.data.key;
+    }
+    var arr = myDiagram.model.nodeDataArray;
+    // choose a random next data object that is not a token and is not the current Node
+    var data = null;
+    while (data === null || data.category === 'token' || data.key === node.data.key) {
+      var i = Math.floor(Math.random() * arr.length);
+      data = arr[i];
+    }
+    // if the data object is a node, return its key
+    return data.key;
+  }
+
+    initTokens();
+    
+    }
+
+
   },
 };
 </script>
